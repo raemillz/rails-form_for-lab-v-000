6421649e31376caa52b83ba3d5810a62b1810f45
Rachel Miller <raeanne1991@gmail.com>
2018-03-20 15:55:01 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,9 @@
 class SchoolClassesController < ApplicationController
 
+  def new
+    @school_class = SchoolClass.new
+  end
+
   def create
     @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
     @school_class.save
