1dd90be183706826926564bdb18925ed7e1895f4
Rachel Miller <raeanne1991@gmail.com>
2018-03-20 16:08:00 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,17 @@
 class StudentsController < ApplicationController
 
+  def new
+    @student = Student.new
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(params.require(:students).permit(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
 end
