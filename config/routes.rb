cd9745eb3d75f6c6f5a146f107a4d71ce32fc293
Rachel Miller <raeanne1991@gmail.com>
2018-03-20 16:19:25 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :school_classes, only: [:new, :create, :show, :edit]
+  resources :school_classes, only: [:new, :create, :show, :edit, :update]
   resources :students, only: [:new, :create, :show]
 end
